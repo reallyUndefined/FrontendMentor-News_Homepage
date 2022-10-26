079e6ce05027794bb5583b8a09afec6a41a6eca2
Prabu <prabu.undefined@gmail.com>
2022-10-26 14:19:17 +0530

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      colors: {
+        softOrange: "hsl(35, 77%, 62%)",
+        softRed: "hsl(5, 85%, 63%)",
+        offWhite: "hsl(36, 100%, 99%)",
+        grayishBlue: "hsl(233, 8%, 79%)",
+        darkGrayishBlue: "hsl(236, 13%, 42%)",
+        veryDarkBlue: "hsl(240, 100%, 5%)",
+      },
     },
   },
   plugins: [],
